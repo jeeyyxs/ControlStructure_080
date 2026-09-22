f940321ecc36617cffb464d598fca0df6e138dba
jeeyyxs <riodwimulyono0512@gmail.com>
2026-09-22 11:11:01 +0700

    code soal nomer 4

---
+++ soal4.py
@@ -0,0 +1,4 @@
+#Write a PYTHON program to print odd numbers up to n!
+n = int(input("Enter the value of odd n: "))
+for steps in range(1, n + 1, 2):
+    print(steps, end=" ") #end untuk memberi spasi agar tidak pindah baris
